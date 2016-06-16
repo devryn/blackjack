daa3bbbb7091d8f2acf91de31d19be0c02f22bbe
Kate Walters <kawalters01@gmail.com>
2016-06-15 19:46:55 -0500

    Version... 5?

--- blackjack.rb
+++ blackjack.rb
@@ -1,26 +1,12 @@
 
-  puts "Welcome to Kate's Jacked Game of Blackjack! Would you like to play?"
-  play = gets.chomp.downcase
-
-  def play_game
-  puts "Draw two cards."
-
-  if play == yes
-    play_game
-  if play == no
-    puts "Sorry to hear that. Come back when you want to play!"
-  else
-    puts "Kate's Jacked Game of Blackjack doesn't understand. Please enter yes or no."
-  end
-  end
-end
 
 class Cards
-  attr_reader :suits, :rank
+  attr_reader :suit, :rank
   def initialize(suit,rank)
     @suit = suit
     @rank = rank
   end
+#to_s here?
 end
 
 class Deck
@@ -31,27 +17,81 @@ class Deck
     suits = [:hearts, :diamonds, :spades, :clubs]
     suits.each do |suit|
       (2..14).each do |rank|
-        @cards << Cards.new(suit, rank)
-      end
-      def shuffle
-        @cards.shuffle!
+        @cards.push(Cards.new(suit, rank))
       end
     end
   end
+  def shuffle
+    @cards.shuffle!
+  end
+  def draw
+    @cards.shift
+  end
 end
+
 
 
 class Hand
-  def attr_accessor
-
+  def run
+    deck = Deck.new
+    deck.shuffle
+  end
 end
+#     puts "Welcome to the game of War."
+#     print ">"
+#     response = gets.chomp.downcase
+#     exit if response == 'q'
+#   end
+# end
+
+
 
 
 class Player
-  attr_reader :score, :name
+  #player score, player card, etc.
 end
 
 
-  class Dealer
+class Dealer
+  #computer score, computer card, etc.
+end
+
+#build game first, then add player/dealer classes
+def run
+  deck = Deck.new
+  deck.make_deck
+  deck.shuffle
+
+
+
+  player_card = []
+  computer_card = []
+
+  player_card.push deck.draw
+  computer_card.push deck.draw
+
+  player_card.push deck.draw
+  computer_card.push deck.draw
+
+puts player_card.to_s
+puts computer_card.to_s
+
+puts "Would you like to hit or stay?"
+response = gets.chomp.downcase
+  #print
+  #gets.chomp
+  #hit or stay?
+
+  #if >17, if < 17
+end
+
+  puts "Welcome to Kate's Jacked Game of Blackjack! Would you like to play?"
+  play = gets.chomp.downcase
+
+  if play == "yes"
+    run
+  elsif play == "no"
+    puts "Sorry to hear that. Come back when you want to play!"
+  else
+    puts "Kate's Jacked Game of Blackjack doesn't understand. Please enter yes or no."
   end
-end
